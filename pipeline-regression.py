85e7b36c37fa295fe6a95abbccab3860b0aea0fc
A.Morin <axelmorinmail@gmail.com>
2026-03-17 17:37:55 +0100

    adapt regression-pipeline to dicussion alexandre-axel

--- pipeline-regression.py
+++ pipeline-regression.py
@@ -1,95 +1,42 @@
 import os
 import json 
 
-import pandas as pd 
-from sklearn.metrics import f1_score
-from torch import load
+from regression import perform_regression, prepare_data
 
-from regression import perform_regression
-
-def prepare_data(path_result: str, model_iteration: str):
-    """
-    Load labels and join metadata and create dummies for regression
-    Return metadata from the label
-    """
-    # Load df labels
-    df_labels = pd.concat([pd.read_csv(f"{path_result}/{model_iteration}/{filename}")
-        for filename in os.listdir(f"{path_result}/{model_iteration}/")
-        if filename.endswith(".csv") and filename.startswith("test_labels-")
-    ])
-
-    with open(f"{path_result}/{model_iteration}/DataHandler_config.json", "r") as file:
-        label_dichotomized = json.load(file)["label_column"]
-    df_labels = df_labels.rename(columns={
-        "LABEL-GS" : f"{label_dichotomized}-GS",
-        "LABEL-PRED" : f"{label_dichotomized}-PRED",
-    })
-    x_columns_list = [f"{label_dichotomized}-GS",f"{label_dichotomized}-PRED"]
-
-    # Load metadata df
-    df_metadata = pd.read_csv("../Article-Bias-Prediction/data_agg.csv")
-    # Join dfs
-    joined_df = (
-        df_labels
-        .set_index("ID")
-        .join(df_metadata.set_index("ID")[["topic", "source"]])
-    )
-    # Create dummies: 
-    dummy_columns = {}
-    for topic in joined_df["topic"].unique():
-        dummy_columns[f"T-{topic}"] = (joined_df["topic"] == topic).copy()
-    for source in joined_df["source"].unique():
-        dummy_columns[f"S-{source}"] = (joined_df["source"] == source).copy()
-    joined_df = pd.concat((joined_df, pd.DataFrame(dummy_columns)), axis = 1)
-
-    # Load metadata from the label
-    with open(f"{path_result}/{model_iteration}/scores.json", "r") as file:
-        scores = json.load(file)
-    best_epoch, best_score = sorted(scores.items(), key = lambda x : x[1], reverse = True)[0]
-    training_args = load(f"{PATH_RESULT}/{model_iteration}/training_args.bin", weights_only=False)
-    learning_rate = training_args.learning_rate
-    weight_decay = training_args.weight_decay
-    model_metadata = {
-        "learning_rate": learning_rate, 
-        "weight_decay": weight_decay, 
-        "best_epoch": best_epoch, 
-        "best_score": best_score, 
-    }
-
-    return joined_df, x_columns_list, model_metadata
-
-PATH_RESULT = "./results/ideology_news_dichotomized"
+PATH_RESULT = "./results"
+DATASET_NAME = "ideology_news_dichotomized"
 MODEL_NAME = "answerdotai/ModernBERT-base"
 full_results = {}
 counter = 0
-for model_iteration in os.listdir(PATH_RESULT):
+
+for model_iteration in os.listdir(f"{PATH_RESULT}/{DATASET_NAME}"):
+
     if not os.path.isdir(f"{PATH_RESULT}/{model_iteration}"): continue  # Skip for non directories
     
-    df, x_columns_list, model_metadata = prepare_data(PATH_RESULT, model_iteration)
-    # create_dummies:
-    for topic in df["topic"].unique():
-        df.loc[:,f"T-{topic}"] = (df["topic"] == topic).copy()
-    for source in df["source"].unique():
-        df.loc[:,f"S-{source}"] = (df["source"] == source).copy()
+    (
+        df, 
+        dependant_variables_columns,
+        independant_variables_columns, 
+        model_metadata
+    ) = prepare_data(PATH_RESULT, model_iteration)
 
-    score_on_current_sample = f1_score(
-        y_true=df[x_columns_list[0]].to_numpy(),
-        y_pred=df[x_columns_list[1]].to_numpy(),
-        average="macro"
-    )
-
-    y_columns_list = [col for col in df.columns if col.startswith("T-")] + [col for col in df.columns if col.startswith("S-")]
-    for x_column in x_columns_list:
-        for y_column in y_columns_list:
+    for dependant_variable in dependant_variables_columns:
+        for independant_variable in independant_variables_columns:
             full_results[counter] = {
-                **perform_regression(df = df, y_column = y_column, x_column = x_column),
-                "x_column" : x_column,
-                "y_column" : y_column,
+                "R-ID" : f"Reg-{counter:06}",
+                "task" : f"{DATASET_NAME}-{model_metadata['label_dichotomized']}",
+                "hypothesis": f"{dependant_variable}~{independant_variable}",
                 "model" : MODEL_NAME,
-                "Score on sample": score_on_current_sample,
-                **model_metadata
+                **model_metadata,
+                "x_column" : independant_variable,
+                "y_column" : dependant_variable,
+                **perform_regression(
+                    df = df, 
+                    x_column = independant_variable,
+                    y_column = dependant_variable, 
+                ),
             }
             counter += 1
 
-with open(f"{PATH_RESULT}/full_results.json", "w") as file:
+with open(f"{PATH_RESULT}/{DATASET_NAME}-regression-results.json", "w") as file:
     json.dump(full_results, file, indent = 4, ensure_ascii = True)
--- regression/regression.py
+++ regression/regression.py
@@ -1,5 +1,10 @@
+import os
+import json 
+
 import pandas as pd 
+from sklearn.metrics import f1_score
 import statsmodels.api as sm
+from torch import load
 
 def perform_regression(df: pd.DataFrame, y_column: str, x_column: str
 ) -> dict[str:str|float|list[float]]:
@@ -46,3 +51,81 @@ def perform_regression(df: pd.DataFrame, y_column: str, x_column: str
             "N obs": "FAILED",
             "N iterations": "FAILED",
         } 
+    
+def prepare_data(path_result: str, model_iteration: str
+) -> tuple[pd.DataFrame, list[str], list[str], dict[str:float|int|str]]:
+    """
+    Load labels and join metadata and create dummies for regression
+
+    The folder {path_result}/{model_iteration}/ must contain the following: 
+        - test_labels-0000.csv : with the ID column, a LABEL-GS and LABEL-PRED column
+        - ...
+        - test_labels-XXXX.csv : with the ID column, a LABEL-GS and LABEL-PRED column
+        - DataHandler_config.json: with at least the "label_column"
+        - scores.json: a dictionary binding the epoch to the F1 score of the model on the testset
+        - training_args.bin: the file with the training arguments of the model
+
+    Return dataframe with the joined dummies, the name of the dependant variables, 
+    the name for the independant variables (dummies) and metadata from the label
+    """
+    # Load df labels
+    df_labels = pd.concat([pd.read_csv(f"{path_result}/{model_iteration}/{filename}")
+        for filename in os.listdir(f"{path_result}/{model_iteration}/")
+        if filename.endswith(".csv") and filename.startswith("test_labels-")
+    ])
+
+    with open(f"{path_result}/{model_iteration}/DataHandler_config.json", "r") as file:
+        label_dichotomized = json.load(file)["label_column"]
+    df_labels = df_labels.rename(columns={
+        "LABEL-GS" : f"{label_dichotomized}-GS",
+        "LABEL-PRED" : f"{label_dichotomized}-PRED",
+    })
+    dependant_variables_columns = [f"{label_dichotomized}-GS",f"{label_dichotomized}-PRED"]
+
+    # Load metadata df
+    df_metadata = pd.read_csv("../Article-Bias-Prediction/data_agg.csv")
+    # Join dfs
+    joined_df = (
+        df_labels
+        .set_index("ID")
+        .join(df_metadata.set_index("ID")[["topic", "source"]])
+    )
+    # Create dummies: 
+    dummy_columns = {}
+    for topic in joined_df["topic"].unique():
+        dummy_columns[f"T-{topic}"] = (joined_df["topic"] == topic).copy()
+    for source in joined_df["source"].unique():
+        dummy_columns[f"S-{source}"] = (joined_df["source"] == source).copy()
+    independant_variables_columns = list(dummy_columns.keys())
+    joined_df = pd.concat((joined_df, pd.DataFrame(dummy_columns)), axis = 1)
+
+    # Load metadata from the label
+    with open(f"{path_result}/{model_iteration}/scores.json", "r") as file:
+        scores = json.load(file)
+    best_epoch, best_score = sorted(scores.items(), key = lambda x : x[1], reverse = True)[0]
+    training_args = load(f"{path_result}/{model_iteration}/training_args.bin", weights_only=False)
+    learning_rate = training_args.learning_rate
+    weight_decay = training_args.weight_decay
+
+    # Evaluate F1 score on samble: 
+    score_on_current_sample = f1_score(
+        y_true = joined_df[f"{label_dichotomized}-GS"].to_numpy(),
+        y_pred = joined_df[f"{label_dichotomized}-PRED"].to_numpy(),
+        average="macro"
+    )
+
+    model_metadata = {
+        "label_dichotomized": label_dichotomized, 
+        "learning_rate": learning_rate, 
+        "weight_decay": weight_decay, 
+        "best_epoch": best_epoch, 
+        "best_score": best_score, 
+        "score_on_sample": score_on_current_sample,
+    }
+
+    return (
+        joined_df, 
+        dependant_variables_columns,
+        independant_variables_columns,
+        model_metadata
+    )
